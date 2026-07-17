a085c62d3977ceb65c31496a800af57d6379795d
paulmygame <lapicerospaul@gmail.com>
2026-07-17 17:45:41 +0800

    core- added weather model

--- SampleProject/Models/WeatherModel.swift
+++ SampleProject/Models/WeatherModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct WeatherResponse: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+    let humidity: Int
+}
+
+struct Weather: Codable {
+    let description: String
+    let icon: String
+}
