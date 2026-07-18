8c4ebbc16da28146d7f26e81d4a7465d291695f9
paulmygame <lapicerospaul@gmail.com>
2026-07-18 12:45:20 +0800

    core - added splash screen

--- SampleProject/View/SplashScreen/SplashScreenView.swift
+++ SampleProject/View/SplashScreen/SplashScreenView.swift
@@ -5,4 +5,31 @@
 //  Created by Paul Eduard Lapiceros on 7/18/26.
 //
 
-import Foundation
+import UIKit
+
+class SplashScreenView: UIViewController {
+    
+    private let logoView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "food_logo")
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.addSubview(logoView)
+        
+        logoView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalToSuperview().multipliedBy(0.3)
+            make.height.equalToSuperview().multipliedBy(0.1)
+        }
+    }
+}
